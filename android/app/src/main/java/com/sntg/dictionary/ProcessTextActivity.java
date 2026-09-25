d4f9ac45da3104e69835995943d637e03d2fb7e9
Sed Nour <30774472+SedNur@users.noreply.github.com>
2026-09-25 21:11:14 +0330

    Update ProcessTextActivity.java

--- android/app/src/main/java/com/sntg/dictionary/ProcessTextActivity.java
+++ android/app/src/main/java/com/sntg/dictionary/ProcessTextActivity.java
@@ -19,11 +19,9 @@ public class ProcessTextActivity extends BridgeActivity {
         // همین Activity را ببندد (چون این یک پنجرهٔ معمولی است، نه تب مرورگر).
         getBridge().getWebView().addJavascriptInterface(new Object() {
             @JavascriptInterface
-public void close() {
-    runOnUiThread(() -> {
-        moveTaskToBack(true);
-    });
-}
+            public void close() {
+                runOnUiThread(ProcessTextActivity.this::finish);
+            }
         }, "AndroidPopup");
 
         // فقط همین یک‌بار (اولین ساخته‌شدنِ Activity) کل صفحه را بارگذاری
